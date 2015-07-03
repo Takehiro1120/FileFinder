7707a4f0206426132894181b1c6d9b92159500e5
Takehiro1120 <Takehiro@KTsMBA.local>
2015-07-03 10:18:09 +0900

    #4 コマンドラインオプション完成
    完成しました。

--- src/main/java/com/github/finder/Main.java
+++ src/main/java/com/github/finder/Main.java
@@ -1,10 +1,16 @@
 package com.github.finder;
 
 public class Main{
-    public Main(String[] arguments){
-    }
-    
-    public static void main(String[] args){
-        new Main(args);
-    }
+	public Main(String[] arguments){
+		Args args = new Args();
+		try {
+			CmdLineParser parser = new CmdLineParser(args);
+			parser.parseArgument(arguments);
+		} catch (CmdLineException e) {
+		}
+	}
+	
+	public static void main(String[] args){
+		new Main(args);
+	}
 }
